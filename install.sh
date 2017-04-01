f38632f0517c609b686615a5a0ffb3a86edb3424
sharonrh <sharon.rhe@gmail.com>
2017-04-01 15:16:18 +0700

    add very basic installation script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# TODO: prints warning & gives prompt to continue
+# TODO: remove if file exists, replacing it with symbolic link
+
+ln -s ~/dotfiles/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
+ln -s ~/dotfiles/'Package Control.sublime-settings' ~/.config/sublime-text-3/Packages/User/'Package Control.sublime-settings'
+ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.bash_profile ~/.bash_profile
