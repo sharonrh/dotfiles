8c446542f642f680f9e3254bcb3a696013ac42a3
sharonrh <sharon.rhe@gmail.com>
2019-08-01 14:27:12 +0700

    symlink vim, add annotation

--- install.sh
+++ install.sh
@@ -3,12 +3,25 @@
 # TODO: prints warning & gives prompt to continue
 # TODO: remove if file exists, replacing it with symbolic link
 
+# sublime
 ln -s ~/dotfiles/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
 ln -s ~/dotfiles/'Package Control.sublime-settings' ~/.config/sublime-text-3/Packages/User/'Package Control.sublime-settings'
-ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
+
+# git
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+
+# tmux
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+# bash
+ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
+
+# vscode
 ln -s ~/dotfiles/settings.json ~/.config/Code/User/settings.json
 ln -s ~/dotfiles/keybindings.json ~/.config/Code/User/keybindings.json
+
+# vim
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.vim ~/.vim
